d5e4efcbb939e69d481ce9c716dc7aa79db700e4
colinjones19 <36234188+colinjones19@users.noreply.github.com>
2018-02-07 09:41:31 -0500

    Create xaml.cs
    added the code (THE REAL XAML.CS)

---
+++ xaml.cs
@@ -0,0 +1,42 @@
+/*
+ * Colin Jones
+ * Feb 6,2018
+ * U1_01Colin.wpf
+ * Hello World - My first program
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace U2_02_Colin_Jones.wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnclickme_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
+
